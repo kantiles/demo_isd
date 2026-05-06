c27cb9b46618ca07274329bbb86f914f0a221422
Thomas Vroylandt <thomas@kantiles.com>
2026-05-06 15:09:44 +0200

    Modifs FR

--- R/export_data_ojs.R
+++ R/export_data_ojs.R
@@ -6,16 +6,21 @@ library(tidyverse)
 
 # Import ------------------------------------------------------------------
 df_isd_assemble <-
-  read_rds(here::here("../../clients/drees/drees_isd/data/isd_assemble.rds"))
+  read_rds(here::here(
+    "../../clients/drees/_old/drees_isd/data/isd_assemble.rds"
+  ))
 
 # Cleaning ----------------------------------------------------------------
 # national value
 df_isd_filter_ojs_nat <- df_isd_assemble |>
   mutate(value = round(value, 2)) |>
-  filter(niv_geo %in% c("fr_ent") &
-           annee >= 2009 &
-           is_isd == TRUE &
-           isd %in% c("C22", "C23", "C24")) |>
+  filter(
+    niv_geo %in%
+      c("fr_ent") &
+      annee >= 2009 &
+      is_isd == TRUE &
+      isd %in% c("C22", "C23", "C24")
+  ) |>
   select(-is_isd, -niv_geo, -code_geo) |>
   rename(value_nat = value)
 
@@ -31,41 +36,47 @@ df_isd_filter_ojs <- df_isd_assemble |>
       value
     )
   ) |>
-  filter(niv_geo %in% c("dep") &
-           annee >= 2009 &
-           is_isd == TRUE &
-           isd %in% c("C22", "C23", "C24")) |>
+  filter(
+    niv_geo %in%
+      c("dep") &
+      annee >= 2009 &
+      is_isd == TRUE &
+      isd %in% c("C22", "C23", "C24")
+  ) |>
   select(-is_isd, -niv_geo) |>
   left_join(df_isd_filter_ojs_nat, by = join_by(annee, isd, indicator)) |>
   mutate(
     diff = value - value_nat,
     indicator = fct_recode(
       indicator,
-      "Before 28 weeks" = "tx_tr_gde_prema_av28sa",
-      "Before 32 weeks" = "tx_gde_prema_av32sa",
-      "Before 37 weeks" = "tx_prema_av37sa",
-      "Less than 1.5kg" = "part_nais_moins_1500g",
-      "Less than 2.5kg" = "part_nais_moins_2500g",
-      "Under 20 years old" = "part_accouch_m20",
-      "20-24 years old" = "part_accouch_20_24",
-      "25-29 years old" = "part_accouch_25_29",
-      "30-34 years old" = "part_accouch_30_34",
-      "35-39 years old" = "part_accouch_35_39",
-      "40 years old and above" = "part_accouch_40p"
+      "Avant 28 semaines" = "tx_tr_gde_prema_av28sa",
+      "Avant 32 semaines" = "tx_gde_prema_av32sa",
+      "Avant 37 semaines" = "tx_prema_av37sa",
+      "Moins de 1.5kg" = "part_nais_moins_1500g",
+      "Moins de 2.5kg" = "part_nais_moins_2500g",
+      "Moins de 20 ans" = "part_accouch_m20",
+      "20-24 ans" = "part_accouch_20_24",
+      "25-29 ans" = "part_accouch_25_29",
+      "30-34 ans" = "part_accouch_30_34",
+      "35-39 ans" = "part_accouch_35_39",
+      "40 ans et plus" = "part_accouch_40p"
     ),
     isd = fct_recode(
       isd,
-      "Premature births by week of amenorrhea" = "C22",
-      "Births according to child weight" = "C23",
-      "Births according to mother age" = "C24"
+      "Naissances prématurées" = "C22",
+      "Poids de naissance" = "C23",
+      "Âge de la mère" = "C24"
     )
   )
 
 # grid OJS
 facet_data <- geofacet::fr_departements_grid1 |>
   as_tibble() |>
-  left_join(df_isd_filter_ojs |>
-              filter(annee == 2022), by = join_by(code == code_geo))
+  left_join(
+    df_isd_filter_ojs |>
+      filter(annee == 2022),
+    by = join_by(code == code_geo)
+  )
 
 # Export ------------------------------------------------------------------
 # only init version that need transpose
